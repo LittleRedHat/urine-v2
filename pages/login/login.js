c0dc881867769fc30d11b2ba02d5f41107d9494d
谢贤成 <zookeeper@xiexianchengs-MacBook-Pro.local>
2017-09-30 12:08:37 +0800

    add all pages

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,8 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
+    medicare:[
+      {
+        value:'城镇居民医保'
+      },
+      {
+        value:'农村医保'
+      },
+      {
+        value:'没有医保'
+      }
+    ],
+    diabetes:[
+      {
+        value:'没有病史',
+
+      },
+      {
+        value: '1年',
+
+      },
+      {
+        value: '2年',
+
+      },
+      {
+        value: '3年',
+
+      },
+      {
+        value: '4年',
+
+      },
+      {
+        value: '5年以上',
+
+      }
+
+    ],
+    hypertensions:[
+      {
+        value: '没有病史',
+
+      },
+      {
+        value: '1年',
+
+      },
+      {
+        value: '2年',
+
+      },
+      {
+        value: '3年',
+
+      },
+      {
+        value: '4年',
+
+      },
+      {
+        value: '5年以上',
+
+      }
+    ],
+    selectedMedicareIndex:-1,
+    selectedHospitalIndex:-1,
+    selectedDiabeteIndex:-1,
+    selectedhypertensionIndex:-1,
+    
+
+    hospitals:[
+      {
+        value:'九龙医院'
+      },
+      {
+        value:'交通大学附属医院'
+      }
+    ]
   
   },
+  bindMedicareChange:function(e){
+    this.setData({
+      selectedMedicareIndex:e.detail.value   
+    })
+  },
+  bindHospitalChange:function(e){
+    this.setData({
+      selectedHospitalIndex: e.detail.value
+    })
+  },
+  bindDiabeteChange: function (e) {
+    this.setData({
+      selectedDiabeteIndex: e.detail.value
+    })
+  },
+  bindHypertensionChange: function (e) {
+    this.setData({
+      selectedHypertensionIndex: e.detail.value
+    })
+  },
+  
 
   /**
    * 生命周期函数--监听页面加载
